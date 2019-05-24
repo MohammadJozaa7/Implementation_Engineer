ed830df29d8d0897960276800c1bb54eaf2f7bd3
Mohammad Jozaa <m.jozaa@yahoo.com>
2019-05-24 22:37:07 +0300

    add,edit,delete button
    make a progress in add modal

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for,request
 
 app = Flask(__name__)
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
+# app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Home
 @app.route('/')
